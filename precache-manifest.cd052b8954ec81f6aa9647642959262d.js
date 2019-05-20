c39e09b7b4376f5c7a26cacd6d167e2f5f9d2c33
feruzshodyev <feruzshodiyev1009@mail.ru>
2019-05-20 18:54:08 +0500

    Updates

---
+++ precache-manifest.cd052b8954ec81f6aa9647642959262d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0a68029aa9491a65fc8950e35e9b29",
+    "url": "/app-calc/index.html"
+  },
+  {
+    "revision": "adb06441a95e4932933c",
+    "url": "/app-calc/static/css/main.72824b68.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/app-calc/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "adb06441a95e4932933c",
+    "url": "/app-calc/static/js/main.f59db189.chunk.js"
+  },
+  {
+    "revision": "a114e6cb1cf64e3be80c",
+    "url": "/app-calc/static/js/runtime~main.9a17f7b4.js"
+  }
+]);
